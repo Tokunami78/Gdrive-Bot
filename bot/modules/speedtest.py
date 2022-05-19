75d93e22d85c5f92e2adf0dd5139b63c17e30a92
Tokunami78 <105770478+Tokunami78@users.noreply.github.com>
2022-05-19 10:48:00 +0530

    Update speedtest.py

--- bot/modules/speedtest.py
+++ bot/modules/speedtest.py
@@ -15,7 +15,7 @@ def speedtest(update, context):
     test.results.share()
     result = test.results.dict()
     string_speed = f'''
-<b>𝐒𝐞𝐫𝐯𝐞𝐫:</b>
+<b>Server Info</b>
 <b>𝐍𝐚𝐦𝐞:</b> <code>{result['server']['name']}</code>
 <b>𝐂𝐨𝐮𝐧𝐭𝐫𝐲:</b> <code>{result['server']['country']}, {result['server']['cc']}</code>
 <b>𝐒𝐩𝐨𝐧𝐬𝐨𝐫:</b> <code>{result['server']['sponsor']}</code>
